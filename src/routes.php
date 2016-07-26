2b1806b442a7bf7ed2c02432de8498c4c3b7efc9
Vinoth Kannan <vinothkannan@vinkas.com>
2016-07-27 03:44:21 +0530

    middleware added for routes

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,9 @@
 <?php
 
+Route::group(['middlewareGroups' => 'web'], function () {
+
 Route::get('auth', 'Vinkas\Firebase\Auth\Http\AuthController@getAuth')->name('getAuth');
 Route::post('auth', 'Vinkas\Firebase\Auth\Http\AuthController@postAuth')->name('postAuth');
 Route::get('logout', 'Vinkas\Firebase\Auth\Http\AuthController@logout')->name('logout');
+
+});
